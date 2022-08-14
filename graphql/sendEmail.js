3c4055d034a368d6e0eab72898a17830ed581506
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-08-13 14:03:33 -1100

    bug fix update

--- graphql/sendEmail.js
+++ graphql/sendEmail.js
@@ -46,8 +46,8 @@ const sendEmail1 = async (val) => {
     try {
       res(
         new OAuth2(
-          `${process.env.NEXT_PUBLIC_CLIENT_ID}`, // ClientID
-          `${process.env.NEXT_PUBLIC_CLIENT_SECRET}`, // Client Secret
+          `${process.env.NEXT_PUBLIC_CLIENT_ID.trim()}`, // ClientID
+          `${process.env.NEXT_PUBLIC_CLIENT_SECRET.charAt(trim)}`, // Client Secret
           "https://developers.google.com/oauthplayground" // Redirect URL
         )
       );
@@ -57,11 +57,12 @@ const sendEmail1 = async (val) => {
   });
   console.log("oauth2", oauth2Client);
   oauth2Client.setCredentials({
-    refresh_token: `${process.env.NEXT_PUBLIC_REFRESH_TOKEN}`.trim(),
+    refresh_token: `${process.env.NEXT_PUBLIC_REFRESH_TOKEN.trim()}`.trim(),
   });
   console.log("Set credentials");
 
   try {
+    console.log("above await");
     const accessToken = await oauth2Client.getAccessToken();
     console.log("access toke", accessToken);
 
